552928021937dc64cab0407e79cad7fd667f65b3
dlsky <dlsky@DESKTOP-L1H898P>
2018-06-10 21:16:22 +0800

    first test

---
+++ lottery/src/main/java/impl/BallRedFilterImpl.java
@@ -0,0 +1,20 @@
+package impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.BallRedFilter;
+
+public class BallRedFilterImpl {
+	
+	List<BallRedFilter> datas = new ArrayList<BallRedFilter>();
+
+	public List<BallRedFilter> getAll() {
+		return null;
+		
+	}
+	
+	public void saveToDb(List<BallRedFilter> datas) {
+		
+	}
+}
