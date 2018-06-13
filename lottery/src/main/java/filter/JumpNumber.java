a4272ed010ef4308715c7369b58bd97d48cf1184
Administrator <Administrator@MZK-Joe>
2018-06-13 16:20:56 +0800

    t

--- lottery/src/main/java/filter/JumpNumber.java
+++ lottery/src/main/java/filter/JumpNumber.java
@@ -8,13 +8,15 @@ import impl.BallRedFilterImpl;
 
 public class JumpNumber {
 	public void filter(List<Integer> cond, List<List<Integer>> datas, int dataNo) {
-		
+
 		//取出最近第二期的中奖号码
 		BallHistoryImpl impl = new BallHistoryImpl();
 		int[] historys= impl.getHistoryByDataNo(dataNo-2);
-		
+
+		//跳号处理后的结果
 		List<List<Integer>> saveData = new ArrayList<List<Integer>>();
-		
+
+		//每组可能的号码与最近第二期的中奖号码相比较，得到“相同次数”的值
 		for(List<Integer> data: datas) {
 			int count = 0;
 			for(Integer fil : data) {
@@ -25,14 +27,14 @@ public class JumpNumber {
 					}
 				}
 			}
-			
+
 			for(int i: cond) {
 				if (count == i) {
 					saveData.add(data);
 				}
 			}
 		}
-		
+
 		BallRedFilterImpl ballRedFilterImpl = new BallRedFilterImpl();
 		ballRedFilterImpl.saveToDb(saveData);
 	}
--- lottery/src/main/java/impl/BallHistoryImpl.java
+++ lottery/src/main/java/impl/BallHistoryImpl.java
@@ -1,8 +1,26 @@
 package impl;
 
-public class BallHistoryImpl {
+import java.io.IOException;
+
+import org.apache.ibatis.session.SqlSession;
+
+import mapper.BaseDao;
+import model.BallHistory;
+
+public class BallHistoryImpl extends BaseDao {
+
 	public int[] getHistoryByDataNo(int dataNo) {
-		
+	    SqlSession sqlSession = null;
+
+	    try {
+	        sqlSession = getSqlSession();
+            BallHistory ballHistory = sqlSession.getMapper(BallHistory.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            sqlSession.close();
+        }
+
 		int[] historys = new int[6];
 		return historys;
 	}
